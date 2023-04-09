11cac0d7026e42b6069eace16d10d505260b6c2d
ndaemy <yuyaebean@gmail.com>
2023-04-09 19:32:53 +0900

    chore: App.tsx 는 src 폴더 안에 있도록

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
