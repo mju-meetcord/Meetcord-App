99dd5fe359f4d7e46933fd5dd6dbc5851ecd4e66
mywhzy <cmj2393a@naver.com>
2023-04-12 16:35:10 +0900

    chore: SafeAreaProvider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { registerRootComponent } from 'expo';
-import { SafeAreaView, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Meetcord : 만남을 기록하다.</Text>
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <View>
+          <Text>Meetcord : 만남을 기록하다.</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
