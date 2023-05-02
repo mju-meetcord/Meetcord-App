05eb2650ccd5f28e74a4dfa951ca0323e3ef0a9d
mywhzy <cmj2393a@naver.com>
2023-05-02 23:14:21 +0900

    chore: list box 추가해 title 위치 재조정 스타일링

--- src/components/MeetList.tsx
+++ src/components/MeetList.tsx
@@ -2,22 +2,30 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const MeetList = () => {
   return (
-    <>
-      <Text style={styles.boxTitle}>나의 Meet 리스트</Text>
-      <View style={styles.viewTopBox}>
-        <ScrollView></ScrollView>
+    <View style={styles.wrapper}>
+      <View style={styles.listBox}>
+        <Text style={styles.boxTitle}>나의 Meet 리스트</Text>
+        <View style={styles.viewTopBox}>
+          <ScrollView></ScrollView>
+        </View>
       </View>
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'center',
+  },
+  listBox: {
+    width: 325,
+  },
   boxTitle: {
     fontWeight: '500',
     fontSize: 16,
     lineHeight: 24,
     marginBottom: 8,
-    marginLeft: 35,
+    marginLeft: 10,
     marginTop: 35,
   },
   viewTopBox: {
