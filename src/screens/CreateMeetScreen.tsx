31de07f5eaf3cdbe8eddce1f5ce80fa1e031be32
mywhzy <cmj2393a@naver.com>
2023-05-06 03:45:10 +0900

    feat: meet 이미지 icon 및 글자수 텍스트 박스 추가 & safeAreaView 적용

--- src/screens/CreateMeetScreen.tsx
+++ src/screens/CreateMeetScreen.tsx
@@ -5,46 +5,62 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import { Shadow } from 'react-native-shadow-2';
 import BackBtn from '../../assets/back_btn.svg';
+import ProfileIcon from '../../assets/icon_profile.svg';
 
 const CreateMeetScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <>
+    <SafeAreaView edges={['bottom']}>
+      <View style={[styles.statusBarPlaceholder, { height: top }]} />
       <View style={styles.wrapper}>
         <BackBtn style={styles.backBtn} />
         <Text style={styles.title}>새로운 Meet 만들기</Text>
         <Shadow startColor='rgba(0, 0, 0, 0.25)' distance={4} offset={[0, 4]}>
           <View style={styles.meetInfoBox}>
-            <View style={styles.meetImg}></View>
-            <TextInput
-              style={styles.meetNameInput}
-              placeholder='Meet 이름을 입력해주세요.'
-              placeholderTextColor='#676767'
-            ></TextInput>
-            <TextInput
-              style={styles.meetIntroInput}
-              placeholder='우리만의 Meet을 소개해보세요.'
-              placeholderTextColor='#676767'
-            ></TextInput>
+            <ProfileIcon style={styles.meetImg} />
+            <View style={styles.nameInputBox}>
+              <TextInput
+                style={styles.meetNameInput}
+                placeholder='Meet 이름을 입력해주세요.'
+                placeholderTextColor='#676767'
+              />
+              <Text style={styles.txtCount}>7/15</Text>
+            </View>
+            <View style={styles.introInputBox}>
+              <TextInput
+                style={styles.meetIntroInput}
+                placeholder='우리만의 Meet을 소개해보세요.'
+                placeholderTextColor='#676767'
+              />
+              <Text style={styles.txtCount}>11/80</Text>
+            </View>
           </View>
         </Shadow>
         <TouchableOpacity style={styles.meetCreateBtn}>
           <Text style={styles.meetCreateBtnTxt}>Meet 시작하기</Text>
         </TouchableOpacity>
       </View>
-    </>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  statusBarPlaceholder: {
+    backgroundColor: '#E9F1FF',
+  },
   wrapper: {
     alignItems: 'center',
     backgroundColor: '#E9F1FF',
     height: '100%',
   },
   backBtn: {
-    marginTop: 56,
     alignSelf: 'flex-start',
     marginLeft: 25,
   },
@@ -62,27 +78,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   meetImg: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    backgroundColor: '#5470ff',
     marginTop: 26,
+  },
+  nameInputBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 287,
+    height: 35,
+    marginTop: 37,
+    justifyContent: 'space-between',
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
   },
   meetNameInput: {
     color: '#676767',
-    marginTop: 37,
-    width: 287,
-    paddingVertical: 8.5,
     fontSize: 18,
     fontWeight: '700',
-    borderBottomColor: '#000000',
-    borderBottomWidth: 1,
+    width: 208,
+  },
+  txtCount: {
+    color: '#878787',
+    fontWeight: '500',
+    fontSize: 14,
+  },
+  introInputBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 287,
+    height: 35,
+    justifyContent: 'space-between',
   },
   meetIntroInput: {
     color: '#676767',
     fontWeight: '500',
     fontSize: 14,
-    width: 287,
+    width: 188,
     paddingVertical: 8.5,
   },
   meetCreateBtn: {
