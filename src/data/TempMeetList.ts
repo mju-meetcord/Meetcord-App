34690e24bacf2c927e2d926f56b79dd0e34ba421
mywhzy <cmj2393a@naver.com>
2023-05-07 05:52:35 +0900

    chore: meetList 목업데이터 추가

---
+++ src/data/TempMeetList.ts
@@ -0,0 +1,32 @@
+export const TempMeetList = [
+  {
+    id: 1,
+    meetImg: require('assets/meet_img1.png'),
+    meetName: '멋쟁이사슴들처럼',
+    meetIntroduce: '전국구 맛집 탐방 동아리 입니당',
+  },
+  {
+    id: 2,
+    meetImg: require('assets/meet_img2.png'),
+    meetName: '탕수육소스 밴드부',
+    meetIntroduce: '노래 못하는건 참아도 소스 찍먹은 못참아',
+  },
+  {
+    id: 3,
+    meetImg: require('assets/meet_img3.png'),
+    meetName: '명지바람 6조',
+    meetIntroduce: '세기의 천재들이 모여서 만드는 개쩌는 졸작',
+  },
+  {
+    id: 4,
+    meetImg: require('assets/meet_img4.png'),
+    meetName: '무한상사',
+    meetIntroduce: '지금도 집에 가고 싶다?       예!!!!',
+  },
+  {
+    id: 5,
+    meetImg: require('assets/meet_img5.png'),
+    meetName: '더글로리에 진심인 사람들',
+    meetIntroduce: '내가 어떤 심정으로 참고 있는   데!!!!!!!!!!!!!!!!!',
+  },
+];
