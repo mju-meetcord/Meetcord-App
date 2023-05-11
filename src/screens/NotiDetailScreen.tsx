17bef1e828fb3eefce2023c6d1f9a23156a531c1
박민지 <parkminji08@naver.com>
2023-05-11 22:32:35 +0900

    chore : text style feedback 반영 수정2

--- src/screens/NotiDetailScreen.tsx
+++ src/screens/NotiDetailScreen.tsx
@@ -5,23 +5,21 @@ import ImageButton from '../components/ImageButton';
 const NotiDetailScreen = ({ route, navigation }: any) => {
   const backBtnHandle = () => {
     navigation.navigate('Notification');
-    // navigation.pop();
-    console.log('테스트용');
   };
 
   const dummyData = {
     title: 'test123',
-    date: '2023.06.05 (화) 10:00',
+    date: '2023. 06. 05 (화) 10:00',
     message:
       '05월 07일의 운세 총운은 “어이상실” 입니다. 꼭 운이 많이 따르고 적게 따르고를 논하기 보다는 여러 가지로 바쁜 하루가 될 것으로 보이는군요. 몸과 마음이 부산하고 정신이 없는 날입니다. 무엇을 했는지도 모르게 하루가 지나가 버릴 확률이 많이 있습니다. 일이 많고 생각보다 성과는 적어 지치는 마음이 들 수도 있습니다. 하지만 결과가 그리 나쁜 것은 아니고 평소하던만큼의 성과는 거둘 수 있으니 편하게 생각하고 천천히 하나씩 해결해 나가다 보면 어느새 하루 해가 지고 있는 모습을 보게 될 것입니다.',
+    // 테스트용 text
   };
-  // 테스트용 text
 
   return (
     <SafeAreaView
       style={{
-        height: '110%',
-        backgroundColor: '#FFFFFF',
+        height: '100%',
+        backgroundColor: '#E9F1FF',
       }}
       edges={['right', 'left', 'top']}
     >
@@ -29,16 +27,18 @@ const NotiDetailScreen = ({ route, navigation }: any) => {
         <View style={styles.Container}>
           <View style={styles.backBtn}>
             <ImageButton
-              onpress={backBtnHandle}
+              onPress={backBtnHandle}
               source={require('../../assets/backBtn.png')}
-              color='#FFFFFF'
+              color='#E9F1FF'
             ></ImageButton>
+            <Text style={styles.title}>공지사항</Text>
           </View>
-          <Text style={styles.title}>공지사항</Text>
           <Text style={styles.notiTittle}>{route.params.title}</Text>
           <Text style={styles.notiDate}>{dummyData.date}</Text>
         </View>
-        <Text style={styles.notiMessage}>{dummyData.message}</Text>
+        <View style={styles.message}>
+          <Text style={styles.notiMessage}>{dummyData.message}</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -49,41 +49,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#E9F1FF',
     borderBottomWidth: 1,
     borderColor: '#C6C6C6',
-    height: 210,
   },
 
   backBtn: {
     justifyContent: 'flex-start',
-    top: 60,
+    marginTop: 25,
+    marginLeft: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 45,
   },
 
   title: {
     fontSize: 18,
-    left: 50,
-    top: 30,
     fontWeight: 'bold',
   },
 
   notiTittle: {
     fontSize: 20,
-    left: 25,
-    top: 75,
+
     fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft: 25,
   },
 
   notiDate: {
     fontSize: 16,
-    left: 25,
-    top: 85,
     color: '#676767',
+    marginLeft: 25,
+    marginBottom: 14,
+  },
+
+  message: {
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
   },
 
   notiMessage: {
     fontSize: 16,
-    width: '80%',
-    left: 25,
-    right: 25,
-    top: 14,
+    width: '85%',
+    marginTop: 14,
+    marginHorizontal: 25,
   },
 });
 
